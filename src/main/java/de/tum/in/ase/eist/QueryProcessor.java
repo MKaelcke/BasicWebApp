3bf77412b4d68deb75f3daa129fde89568007182
Marina <m.kaelcke@tum.de>
2022-07-13 11:06:17 +0200

    Kommentar

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -14,9 +14,8 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
             return "marina";
+        } else { // TODO extend the programm here
+            if () return "";
         }
-        /*else { // TODO extend the programm here
-            if ()return "";
-        }/*
     }
 }
